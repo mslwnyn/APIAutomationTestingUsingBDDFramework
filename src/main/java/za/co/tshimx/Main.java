d231b0c4e2c6c512ea41f568133a1c1c8de11e32
Lebone <maslwnyn@gmail.com>
2024-09-03 06:18:16 +0200

    Initial commit

---
+++ src/main/java/za/co/tshimx/Main.java
@@ -0,0 +1,7 @@
+package za.co.tshimx;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/za/co/tshimx/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package za.co.tshimx.runner;
+
+
+import org.junit.runner.RunWith;
+
+@RunWith(io.cucumber.junit.Cucumber.class)
+@io.cucumber.junit.CucumberOptions(
+        features = "./src/test/java/za/co/tshimx/features",
+        glue={"za.co.tshimx.stepdefs"},
+        tags = "@api",
+        plugin = {"json:target/cucumber.json","html:target/site/cucumber-pretty"}
+
+
+)
+public class TestRunner {
+
+
+}
+
